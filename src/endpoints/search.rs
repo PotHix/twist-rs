535b45ef94b18f0debe2c326a35a1da69096b303
PotHix <pothix@pothix.com>
2020-10-23 20:01:03 -0300

    Adds the first test WIP

--- src/endpoints/search.rs
+++ src/endpoints/search.rs
@@ -1,7 +1,13 @@
+#[cfg(test)]
+use mockito;
 use reqwest::blocking::Response;
 
 pub fn search(token: String, query: String) -> Result<Response, Box<dyn std::error::Error>> {
+    #[cfg(not(test))]
     let mut url = super::super::URL.to_string();
+    #[cfg(test)]
+    let mut url = mockito::server_url();
+
     url.push_str(super::super::API_VERSION);
     url.push_str("/search/query");
 
@@ -34,4 +40,24 @@ pub fn details(
         .send()?;
 
     return Ok(res);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mockito::mock;
+
+    #[test]
+    #[ignore]
+    fn request_a_search_query() {
+        let _m = mock("GET", "/search/query")
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body("{}")
+            .create();
+
+        if let Ok(res) = search("token".to_string(), "query".to_string()) {
+            assert_eq!(res.text().unwrap(), "{}");
+        }
+    }
 }
