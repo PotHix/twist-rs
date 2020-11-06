93ab8d35981985d3b55dd4e6a87a6aa3acbcf46b
PotHix <pothix@pothix.com>
2020-11-06 17:22:48 -0300

    Remove the additional console.log

--- examples/javascript/lib/index.js
+++ examples/javascript/lib/index.js
@@ -2,7 +2,6 @@ const twist = require('../native/index.node');
 const token = process.env.auth;
 const query = "pothix";
 
-console.log(token);
 console.log(twist.search(token, query));
 
 module.exports = twist;
