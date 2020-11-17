f1c297cc39281bb6752d0485cbad634801883924
PotHix <pothix@pothix.com>
2020-11-17 16:49:21 -0300

    Less `unwrap` and more `expect`
    I just want to be sure I know the reason when something bad happens.

--- src/ffi/search.rs
+++ src/ffi/search.rs
@@ -5,15 +5,25 @@ use std::os::raw::c_char;
 
 #[no_mangle]
 pub extern "C" fn search(token: *mut c_char, query: *mut c_char) -> *mut c_char {
+    // Gets a Cstr from a c_char pointer
     let c_str_token = unsafe { CStr::from_ptr(token) };
+
+    // Transforms the CStr type into a &str
     let str_token = c_str_token.to_str().unwrap();
 
     let c_str_query = unsafe { CStr::from_ptr(query) };
     let str_query = c_str_query.to_str().unwrap();
 
-    let res = endpoints::search::search(str_token.to_string(), str_query.to_string());
+    // Since we use String as arguments for `search` we have to convert the &str to a String (heap
+    // allocated string of variable size) to use as function arguments
+    let res = endpoints::search::search(str_token.to_string(), str_query.to_string())
+        .expect("No valid result returned from endpoints::search::search");
 
-    let s = CString::new(res.unwrap().text().unwrap()).unwrap();
+    let response_text = res
+        .text()
+        .expect("Couldn't convert the response object into text");
+
+    let s = CString::new(response_text).expect("Couldn't convert the response text into a CString");
     return s.into_raw();
 }
 
